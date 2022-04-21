4be26ba7369e4fd5064d71e666bf64e9c1c69dee
Thomas Saji <thomasms213@gmail.com>
2022-04-21 22:00:06 +0530

    fc

--- src/connect/db.js
+++ src/connect/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const db = "mongodb://localhost:27017/react-redux-a-2"
+
 module.exports = () => {
-    return mongoose.connect(db)
+    return mongoose.connect(process.env.CONNECT);
 }
